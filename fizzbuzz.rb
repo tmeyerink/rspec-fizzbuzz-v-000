adbecd5e8c2b53ebc88df75ca850a42f6c8f2420
Tara Meyerink <tarajmeyerink@gmail.com>
2017-10-15 18:55:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(int)
-  if int % 15 == 0
+  if (int % 3 == 0 && int % 5 == 0)
     "FizzBuzz"
   elseif int % 3 == 0
     "Fizz"
   elseif int % 5 == 0
     "Buzz"
+  else
+    return nil
   end
 end
